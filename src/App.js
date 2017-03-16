3521b96f8445b21749e3d4bc61b82f241962afcf
Jeremy J. Johnson <jeremyjohnson7@mail.weber.edu>
2017-03-15 21:04:13 -0600

    Updated code

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <div className="logo">
+                    <h1>LOGO</h1>
+                </div>
+
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <h2>Welcome to React</h2>
@@ -28,12 +32,16 @@ class App extends Component {
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
 
-                <button onClick={this.toggleOpen.bind(this)}></button>
+                <button onClick={this.toggleOpen.bind(this)}>Toggle Modal</button>
 
                 <Modal visible={this.state.modalOpen}>
                     <h1>Hello world</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic eum, impedit fugiat laboriosam ullam autem et, maxime adipisci consectetur ex delectus! Sed voluptate asperiores consectetur recusandae tempora dolorum, perferendis veritatis!</p>
                 </Modal>
+
+                {/*IcoMoon - Icon fonts*/}
+
+
             </div>
         );
     }
